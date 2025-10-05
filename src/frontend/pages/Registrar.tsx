2051151cb2b2aa054cf0008414b724a971747f4e
wcalder <wcalder@uvic.ca>
2025-10-05 16:43:53 -0700

    Some front end updates

--- src/frontend/pages/Registrar.tsx
+++ src/frontend/pages/Registrar.tsx
@@ -5,9 +5,19 @@ const Registrar: React.FC = () => {
     return (
         <div>
             <h2>Classroom Logs</h2>
+
             <h2>Classroom Statistics</h2>
+
             <h2>Account Management</h2>
+            <input className="Search-bar" type="text" id="SearchBar" placeholder="Search for an Account" />
+            <button className="Search-button">Search</button>
+            {/*button onclick="search()">Search</button>*/}
+
             <h2>Classroom Management</h2>
+            <input className="Search-bar" type="text" id="SearchBar" placeholder="Search a Classroom" />
+            <button className="Search-button">Search</button>
+            {/*button onclick="search()">Search</button>*/}
+
         </div>
     );
 };
