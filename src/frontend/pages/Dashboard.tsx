e9f680f0222663411637ecd066340c977014b92f
wcalder <wcalder@uvic.ca>
2025-10-04 17:46:08 -0700

    Baseline front-end hierarchy and code has been added

---
+++ src/frontend/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import '' //This is where we will import the css page to go along with this page.
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Dashboard page</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
