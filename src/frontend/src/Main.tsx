04ee937360ff92a41477a185465c1222af6a8fd3
Max Patchell <maxpatchell@uvic.ca>
2025-11-07 14:21:43 -0800

    fixing main import navigation

--- src/frontend/src/Main.tsx
+++ src/frontend/src/Main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import Dashboard from './pages/Dashboard';
-import SignIn from './pages/SignIn';
-import ClassroomSearchPage from './pages/ClassroomSearchPage';
-import MyBookings from './pages/MyBookings';
-import Admin from './pages/Admin';
-import Registrar from './pages/Registrar';
+import Dashboard from './pages/Dashboard.tsx';
+import SignIn from './pages/SignIn.tsx';
+import ClassroomSearchPage from './pages/ClassroomSearchPage.tsx';
+import MyBookings from './pages/MyBookings.tsx';
+import Admin from './pages/Admin.tsx';
+import Registrar from './pages/Registrar.tsx';
 
 import './styles/Main.css';
 
