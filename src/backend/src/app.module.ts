9e8153bc9c4bed4be2d19e6857d28a08b338e484
Max Patchell <maxpatchell@uvic.ca>
2025-11-07 15:42:42 -0800

    registrar --log fix basic functionality

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -5,13 +5,13 @@ import { AppService } from './app.service';
 import { RoomsModule } from './rooms/rooms.module';
 import { BookingsModule } from './bookings/bookings.module';
 import { UsersModule } from './users/users.module';
+import { LogsModule } from './logs/logs.module';
 import { Room } from './rooms/rooms.entity';
 import { Booking } from './bookings/bookings.entity';
 import { User } from './users/users.entity';
-import { LogsModule } from './logs/logs.module';
+import { Log } from './logs/logs.entity';
 import * as dotenv from 'dotenv';
 dotenv.config();
-
 
 @Module({
   imports: [
@@ -22,10 +22,14 @@ dotenv.config();
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [Room, Booking, User],
+      entities: [Room, Booking, User, Log],
       synchronize: true,
     }),
-    RoomsModule, BookingsModule, UsersModule, LogsModule], 
+    RoomsModule,
+    BookingsModule,
+    UsersModule,
+    LogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
